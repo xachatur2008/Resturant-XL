c8039c0235f8fc3afcaaff8ff06ae1c3108d37d9
Khachatur <xojoyanxachatur2008@gmail.com>
2025-07-18 22:45:55 +0400

    modal is ready

--- js/script.js
+++ js/script.js
@@ -102,9 +102,13 @@ window.addEventListener("DOMContentLoaded", () => {
     const openBtns = document.querySelectorAll('[data-modal]'),
         closeBtn = document.querySelector('[data-close]'),  
         modalWindow = document.querySelector('.modal'),
-        submit = document.querySelector('.btn_dark'),
-        input = document.querySelector('.modal__input')
+        submit = document.querySelector('.mbtn'),
+        NameInput = document.querySelector('.nameInput'),
+        NumInput = document.querySelector(".phone-number"),
+        modalTitle = document.querySelector(".modal__title"),
+        countryCode = document.querySelector(".prefix-select")
 
+    
     function showModal(){
         modalWindow.classList.add('show')
         modalWindow.classList.remove('hide')
@@ -116,6 +120,15 @@ window.addEventListener("DOMContentLoaded", () => {
         modalWindow.classList.add('hide')
         modalWindow.classList.remove('show')
         document.body.style.overflow = ''
+        [NameInput, countryCode, NumInput].forEach(input => {
+            input.classList.remove('hide')
+            if(input.tagName === 'INPUT') input.value = ''
+        })
+
+        submit.classList.remove('hide')
+        modalTitle.textContent = "Заполните форму"
+        modalTitle.style.color = ''
+        modalTitle.style.marginTop = ''
     }
     
 
@@ -132,6 +145,7 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     })
 
+
     document.addEventListener('keydown', (e) =>{
         if(e.code === "Escape" && modalWindow.classList.contains('show')){
             hideModal();
@@ -147,5 +161,56 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    window.addEventListener('scroll', byScroll)
+     // window.addEventListener('scroll', byScroll)
+
+    function fillingApplication(){
+         let data;
+         if(NameInput.value.trim() !== "" && NumInput.value.trim() !== ""){
+            data = {
+                name :NameInput.value,
+                Telephone:NumInput.value
+            }
+            submit.classList.add('hide')
+            NameInput.classList.add('hide')
+            NumInput.classList.add('hide')
+            modalTitle.textContent = "Спасибо за ваш запрос. Мы свяжемся с вами."
+            modalTitle.style.color = 'green';
+            modalTitle.style.marginTop = "30px";
+            console.log("Отправленные данные:", data)
+        }
+    }
+
+    submit.addEventListener('click', fillingApplication)
+
+
+
+
+    // ------------------------OFFER------------------------
+
+    const leftArrow = document.querySelector('.offer__slider-prev'),
+          rightArrow = document.querySelector('.offer__slider-next'),
+          images = document.querySelectorAll('.offer__slider-wrapper') 
+          current = document.querySelector('#current')
+    
+    // let data = {
+    //     image1 : {
+    //         firstImage : images[0],
+    //         order : "01"
+    //     },
+        
+    //     image2 : {
+    //         secondImage : images[1],
+    //         order : "02"
+    //     }
+        
+    // }      
+
+    function left(i){
+        
+
+    }
+    // let current = images[0];
+    leftArrow.addEventListener('click', () => {
+         
+    })
 });
